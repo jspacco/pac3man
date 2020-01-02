60dded3a0d16983941d490bc8e5b160a9dcf755c
Jaime Spacco <jspacco@knox.edu>
2020-01-02 01:05:23 -0600

    added runner for reinforcement for students using Windows

---
+++ reinforcement/run.py
@@ -0,0 +1,12 @@
+import autograder
+import gridworld
+import pacman
+
+autograder.main('python3 -q q1')
+#autograder.main('python3 -q q2')
+#autograder.main('python3 -t test_cases/q1/1-tinygrid')
+
+#gridworld.main('python3 gridworld.py -a q -k 100 -n 0 -m  -l 0.1 -d 0.85 -g VerticalBridgeGrid')
+
+#pacman.main('pacman.py -p PacmanQAgent -x 2000 -n 2010 -l smallGrid')
+
