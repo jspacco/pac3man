3687dd810e9fc0afa220b594e3336e1044360c5b
Jaime Spacco <jspacco@knox.edu>
2020-01-03 00:20:53 -0600

    made progress on graphviz

--- markov/graphit.py
+++ markov/graphit.py
@@ -1,6 +1,9 @@
 import sys
 from babbler import Babbler
 
+numnodes = 1
+names = {}
+
 def frequencies(items):
     counts = {}
     for i in items:
@@ -18,29 +21,51 @@ def percents(items):
         percentages[key] = count // total
     return percentages
 
+def getnode(name):
+    global numnodes
+    global names
+    if name not in names:
+        names[name] = f'N{nodes:03d}'
+        nodes += 1
+    return names[name]
+
 def main(n, filename):
     babbler = Babbler(n)
     babbler.add_file(filename)
     
-    nodes = 1
-    visited = dict()
-
     st = ''
     startpct = percents(babbler.get_starters())
     for starter in startpct.keys():
-        if starter not in visited:
-            visited[starter] = f'N{nodes:03d}'
-            nodes += 1
-        st += f'start -> {visited[starter]} [label = {startpct[starter]:0.2f}];\n'
+        name = getnode(starter)
+        st += f'start -> {name} [label = {startpct[starter]:0.2f}];\n'
 
-    tovisit = list(set(babbler.get_starters()))
+    links = ''
+    tovisit = startpct.keys()
+    visited = set()
+    while len(tovisit) > 0:
+        state = tovisit.pop()
+        if state in visited:
+            continue
+        visited.add(state)
+
+        name = getnode(state)
+        if not babbler.has_successor(state):
+            links += f'{name} -> end [label = 1.0]\n'
+            continue
+
+        successorpct = percents(babbler.get_successors(state))
+        for nextnode in successorpct.keys():
+            if nextnode not in visited:
+                tovisit.append(nextnode)
+            nextname = getnode(nextnode)
+            links += f'{name} -> {nextname} [label = {successorpct[nextnode]:0.2f}];\n'
     
-    #while len(tovisit) > 0:
-        # pop node
-        # if visited, continue
-        # compute transition percentages
-        # 
-    
+    nodes = ''
+    global names
+    for state, name in names.items():
+        nodes += f'{name} [label={state}]\n'
+
+
     # TODO: convert visited into nodes with labels
     # figure out transition labels
 
@@ -51,8 +76,10 @@ digraph finite_state_machine {
 	node [shape = doublecircle]; start end;
 	node [shape = circle];
     %s
+    %s
+    %s
 }
-    """ % (st,)
+    """ % (nodes, st, links)
 
     print(graph)
 
