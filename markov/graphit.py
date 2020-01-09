004ae364c2d683a255bbd2e7cb23d6d344c3ea89
Jaime Spacco <jspacco@knox.edu>
2020-01-08 21:20:44 -0600

    fixed the graphit code

--- markov/graphit.py
+++ markov/graphit.py
@@ -1,6 +1,9 @@
 import sys
 from babbler import Babbler
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 numnodes = 1
 names = {}
 
@@ -18,16 +21,23 @@ def percents(items):
     percentages = {}
     total = sum(freqs.values())
     for key, count in freqs.items():
-        percentages[key] = count // total
+        percentages[key] = count / total
     return percentages
 
 def getnode(name):
     global numnodes
     global names
     if name not in names:
-        names[name] = f'N{nodes:03d}'
-        nodes += 1
+        names[name] = f'N{numnodes:03d}'
+        numnodes += 1
     return names[name]
+
+def fix(state, word):
+    if word is None:
+        return None
+    if ' ' not in state:
+        return word
+    return state[state.index(' ')+1:] + ' ' + word
 
 def main(n, filename):
     babbler = Babbler(n)
@@ -37,44 +47,52 @@ def main(n, filename):
     startpct = percents(babbler.get_starters())
     for starter in startpct.keys():
         name = getnode(starter)
-        st += f'start -> {name} [label = {startpct[starter]:0.2f}];\n'
+        st += f'start -> {name} [label = {startpct[starter]:0.2f} fontsize="36"];\n'
 
     links = ''
-    tovisit = startpct.keys()
+    tovisit = list(startpct.keys())
     visited = set()
     while len(tovisit) > 0:
-        state = tovisit.pop()
+        state = tovisit.pop(0)
+        eprint(f'state {state}')
         if state in visited:
             continue
         visited.add(state)
 
         name = getnode(state)
         if not babbler.has_successor(state):
-            links += f'{name} -> end [label = 1.0]\n'
+            links += f'{name} -> end [label = 1.0 fontsize="36"];\n'
             continue
 
-        successorpct = percents(babbler.get_successors(state))
-        for nextnode in successorpct.keys():
-            if nextnode not in visited:
-                tovisit.append(nextnode)
-            nextname = getnode(nextnode)
-            links += f'{name} -> {nextname} [label = {successorpct[nextnode]:0.2f}];\n'
+        eprint(f'{state} successors: {babbler.get_successors(state)}')
+        
+        successors = [(word, fix(state, word)) for word in babbler.get_successors(state)]
+        eprint(f'successors of {state}: {successors}')
+        
+        successorpct = percents(successors)
+        eprint(f'successorpct {successorpct}')
+        
+        for (word, nextstate), weight in successorpct.items():
+            if word == 'EOL':
+                links += f'{name} -> end [label = "{weight:.2f}" fontsize="36"];\n'
+                continue
+            nextname = getnode(nextstate)
+            if nextname not in visited:
+                tovisit.append(nextstate)
+            links += f'{name} -> {nextname} [label = "{word}\\n{weight:0.2f}" fontsize="36"];\n'
     
     nodes = ''
     global names
     for state, name in names.items():
-        nodes += f'{name} [label={state}]\n'
+        nodes += f'{name} [label="{state}"];\n'
 
-
-    # TODO: convert visited into nodes with labels
-    # figure out transition labels
 
     graph = """
 digraph finite_state_machine {
 	rankdir=LR;
-	#size="8,5"
-	node [shape = doublecircle]; start end;
-	node [shape = circle];
+	size="8,5"
+	node [shape = doublecircle fontsize="36"]; start end;
+	node [shape = circle fontsize="36"];
     %s
     %s
     %s
