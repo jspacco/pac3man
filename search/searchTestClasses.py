95f3b27fc705ce0074b2da5ddbf13937cede6eb5
Jaime Spacco <jspacco@knox.edu>
2020-01-15 23:04:39 -0600

    python2 to python3 porting of map function

--- search/searchTestClasses.py
+++ search/searchTestClasses.py
@@ -69,7 +69,7 @@ class GraphSearch(SearchProblem):
             print('"""%s"""' % graph_text)
             raise Exception("GraphSearch graph specification goal_states not found or incorrect on line:" + l)
         goals = r.group(1).split()
-        self.goals = map(str.strip, goals)
+        self.goals = list(map(str.strip, goals))
         self.successors = {}
         all_states = set()
         self.orderedSuccessorTuples = []
@@ -770,7 +770,7 @@ class CornerHeuristicPacman(testClasses.TestCase):
         searchAgents = moduleDict['searchAgents']
         total = 0
         true_cost = float(solutionDict['cost'])
-        thresholds = map(int, solutionDict['thresholds'].split())
+        thresholds = list(map(int, solutionDict['thresholds'].split()))
         game_state = pacman.GameState()
         lay = layout.Layout([l.strip() for l in self.layout_text.split('\n')])
         game_state.initialize(lay, 0)
