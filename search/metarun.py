7bcbd9db0e5daa7d59eca7a44bbd49532f298874
Jaime Spacco <jspacco@knox.edu>
2020-01-18 12:13:18 -0600

    working on run.py

--- search/metarun.py
+++ search/metarun.py
@@ -1,15 +1,16 @@
 import pacman
 
-'''
-metarun.py runs things that look like command-line arguments
+"""
+run.py runs things that look like command-line arguments
 for Berkeley Python. You can leave the 'python pacman.py' part
 at the beginning, or remove it, since we are not really running
 from the command line.
 
-You should comment out all lines in the file except one!
-'''
+You should comment out all lines in the file except the one
+you wan to run!
+"""
 
-#pacman.main('--layout tinyMaze --pacman GoWestAgent')
-#pacman.main('-l tinyMaze -p SearchAgent -a fn=tinyMazeSearch')
+#pacman.main('python pacman.py --layout tinyMaze --pacman GoWestAgent')
+#pacman.main('python pacman.py -l tinyMaze -p SearchAgent -a fn=tinyMazeSearch')
 #pacman.main('python pacman.py -l mediumMaze -p SearchAgent -a fn=dfs')
-pacman.main('python pacman.py -t test_cases/q2/1-bridge-grid')
+pacman.main('python autograder.py -t test_cases/q2/1-bridge-grid')
