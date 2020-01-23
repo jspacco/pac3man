579637513ea7d65e613f265fcc3e5dfac33dc444
Jaime Spacco <jspacco@knox.edu>
2020-01-23 13:36:48 -0600

    python3.8 removed cgi.escape, so we have replaced it with html.escape

--- search/grading.py
+++ search/grading.py
@@ -14,7 +14,7 @@
 
 "Common code for autograders"
 
-import cgi
+import html
 import time
 import sys
 import json
@@ -291,7 +291,7 @@ to follow your instructor's guidelines to receive credit on your project.
         if self.mute: util.unmutePrint()
         print('*** ' + message)
         if self.mute: util.mutePrint()
-        message = cgi.escape(message)
+        message = html.escape(message)
     self.messages[self.currentQuestion].append(message)
 
   def addMessageToEmail(self, message):
